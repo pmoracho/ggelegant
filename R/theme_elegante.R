a0bb2ae9017bd6a42d034c58f598f02cc7a66bb0
Patricio Moracho <pmoracho@gmail.com>
2019-09-01 12:32:49 -0300

    Primer commit funcional. Se usan los fonts base de ggplot2.

--- R/theme_elegante.R
+++ R/theme_elegante.R
@@ -6,7 +6,7 @@
 #' @return ggplot theme
 #'
 #' @examples
-#' library(ggplot2)
+#' library("ggplot2")
 #'
 #' ggplot(mtcars, aes(x = mpg*0.43, y = wt*0.4535924, colour = factor(cyl))) +
 #'   geom_point(size = 2) +
@@ -16,8 +16,6 @@
 #'        y = "Weight (1000 kg)",
 #'        colour = "Cylinders") +
 #'   theme_elegante_std()
-#'
-#' @seealso \code{\link{theme_elegante}}
 #'
 #' @export
 theme_elegante_std <- function(base_size = 10,
@@ -33,92 +31,61 @@ theme_elegante_std <- function(base_size = 10,
     strip.background.color = '#9999CC'
 
     ret <-
-        theme_bw(base_size=base_size) +
-
+        ggplot2::theme_bw(base_size=base_size) +
         # Set the entire chart region to a light gray color
-        theme(panel.background=element_rect(fill=color.background, color=color.background)) +
-        theme(plot.background=element_rect(fill=color.background, color=color.background)) +
-        theme(panel.border=element_rect(color=color.background)) +
+        ggplot2::theme(panel.background=ggplot2::element_rect(fill=color.background, color=color.background)) +
+        ggplot2::theme(plot.background=ggplot2::element_rect(fill=color.background, color=color.background)) +
+        ggplot2::theme(panel.border=ggplot2::element_rect(color=color.background)) +
 
         # Format the grid
-        theme(panel.grid.major=element_line(color=color.grid.major,size=.55, linetype="dotted")) +
-        theme(panel.grid.minor=element_line(color=color.grid.major,size=.55, linetype="dotted")) +
-        theme(axis.ticks=element_blank()) +
+        ggplot2::theme(panel.grid.major=ggplot2::element_line(color=color.grid.major,size=.55, linetype="dotted")) +
+        ggplot2::theme(panel.grid.minor=ggplot2::element_line(color=color.grid.major,size=.55, linetype="dotted")) +
+        ggplot2::theme(axis.ticks=ggplot2::element_blank()) +
 
         # Format the legend, but hide by default
-        theme(legend.position="none") +
-        theme(legend.background = element_rect(fill=color.background)) +
-        theme(legend.text = element_text(size=base_size-3,color=color.axis.title, family = base_family)) +
+        ggplot2::theme(legend.position="none") +
+        ggplot2::theme(legend.background = ggplot2::element_rect(fill=color.background)) +
+        ggplot2::theme(legend.text = ggplot2::element_text(size=base_size-3,color=color.axis.title, family = base_family)) +
 
-        theme(strip.text.x = element_text(size=base_size,color=color.background, family = base_family)) +
-        theme(strip.text.y = element_text(size=base_size,color=color.background, family = base_family)) +
-        theme(strip.background = element_rect(fill = "grey70", colour = NA)) +
+        ggplot2::theme(strip.text.x = ggplot2::element_text(size=base_size,color=color.background, family = base_family)) +
+        ggplot2::theme(strip.text.y = ggplot2::element_text(size=base_size,color=color.background, family = base_family)) +
+        ggplot2::theme(strip.background = ggplot2::element_rect(fill = "grey70", colour = NA)) +
         # theme(panel.border= element_rect(fill = NA, colour = "grey70", size = rel(1)))+
         # Set title and axis labels, and format these and tick marks
-        theme(plot.title=element_text(color=color.title,
-                                      size=20,
-                                      vjust=1.25,
-                                      family=base_family,
-                                      hjust = 0.5,
-                                      face = "bold"
-                                      )) +
+        ggplot2::theme(plot.title=ggplot2::element_text(color=color.title,
+                                                        size=20,
+                                                        vjust=1.25,
+                                                        family=base_family,
+                                                        hjust = 0.5,
+                                                        face = "bold"
+                                                        )) +
 
-        theme(plot.subtitle=element_text(color=color.subtitle, size=base_size+2, family = base_family,  hjust = 0.5))  +
+        ggplot2::theme(plot.subtitle=ggplot2::element_text(color=color.subtitle, size=base_size+2, family = base_family,  hjust = 0.5))  +
 
-        theme(axis.text.x=element_text(size=base_size,color=color.axis.text, family = base_family)) +
-        theme(axis.text.y=element_text(size=base_size,color=color.axis.text, family = base_family)) +
-        theme(text=element_text(size=base_size, color=color.axis.text, family = base_family)) +
+        ggplot2::theme(axis.text.x=ggplot2::element_text(size=base_size,color=color.axis.text, family = base_family)) +
+        ggplot2::theme(axis.text.y=ggplot2::element_text(size=base_size,color=color.axis.text, family = base_family)) +
+        ggplot2::theme(text=ggplot2::element_text(size=base_size, color=color.axis.text, family = base_family)) +
 
-        theme(axis.title.x=element_text(size=base_size+2,color=color.axis.title, vjust=0, family = base_family)) +
-        theme(axis.title.y=element_text(size=base_size+2,color=color.axis.title, vjust=1.25, family = base_family)) +
-        theme(plot.caption=element_text(size=base_size-2,color=color.axis.title, vjust=1.25, family = base_family)) +
+        ggplot2::theme(axis.title.x=ggplot2::element_text(size=base_size+2,color=color.axis.title, vjust=0, family = base_family)) +
+        ggplot2::theme(axis.title.y=ggplot2::element_text(size=base_size+2,color=color.axis.title, vjust=1.25, family = base_family)) +
+        ggplot2::theme(plot.caption=ggplot2::element_text(size=base_size-2,color=color.axis.title, vjust=1.25, family = base_family)) +
 
         # Legend
-        theme(legend.text=element_text(size=base_size,color=color.axis.text, family = base_family)) +
-        theme(legend.title=element_text(size=base_size,color=color.axis.text, family = base_family)) +
-        theme(legend.key=element_rect(colour = color.background, fill = color.background)) +
-        theme(legend.position="bottom",
-              legend.box = "horizontal",
-              legend.title = element_blank(),
-              legend.key.width = unit(.75, "cm"),
-              legend.key.height = unit(.75, "cm"),
-              legend.spacing.x = unit(.25, 'cm'),
-              legend.spacing.y = unit(.25, 'cm'),
-              legend.margin = margin(t=0, r=0, b=0, l=0, unit="cm")) +
+        ggplot2::theme(legend.text=ggplot2::element_text(size=base_size,color=color.axis.text, family = base_family)) +
+        ggplot2::theme(legend.title=ggplot2::element_text(size=base_size,color=color.axis.text, family = base_family)) +
+        ggplot2::theme(legend.key=ggplot2::element_rect(colour = color.background, fill = color.background)) +
+        ggplot2::theme(legend.position="bottom",
+                       legend.box = "horizontal",
+                       legend.title = ggplot2::element_blank(),
+                       legend.key.width = ggplot2::unit(.75, "cm"),
+                       legend.key.height = ggplot2::unit(.75, "cm"),
+                       legend.spacing.x = ggplot2::unit(.25, 'cm'),
+                       legend.spacing.y = ggplot2::unit(.25, 'cm'),
+                       legend.margin = ggplot2::margin(t=0, r=0, b=0, l=0, unit="cm")
+                       ) +
 
         # Plot margins
-        theme(plot.margin = unit(c(.5, .5, .5, .5), "cm"))
+        ggplot2::theme(plot.margin = ggplot2::unit(c(.5, .5, .5, .5), "cm"))
 
     ret
-}
-
-#' Elegante theme
-#'
-#' @param base_size Change the font base size. Default to 10
-#' @param base_family Change the font family. Defaults to standard Raleway
-#'
-#' @return ggplot theme
-#'
-#' @examples
-#' library(ggplot2)
-#'
-#' ggplot(mtcars, aes(x = mpg*0.43, y = wt*0.4535924, colour = factor(cyl))) +
-#'   geom_point(size = 2) +
-#'   labs(title = "Car weight vs efficiency",
-#'        subtitle = "Using sensible metrics",
-#'        x = "Efficiency (km/l)",
-#'        y = "Weight (1000 kg)",
-#'        colour = "Cylinders") +
-#'   theme_elegante()
-#'
-#' @seealso \code{\link{theme_elegante}}
-#'
-#' @export
-theme_elegante <- function(base_size = 10,
-                           base_family = "Raleway"
-)
-{
-    theme_elegante_std(base_size = base_size,
-                        base_family = base_family)
-
 }
